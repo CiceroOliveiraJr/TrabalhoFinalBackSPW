d5755d12505b5d4692118f9124471cfaa3e70105
CiceroOliveiraJr <cicero_jbg@hotmail.com>
2020-10-18 10:52:25 -0300

    configuracão da classe Cliente

--- src/main/java/com/lojaunit/base/Cliente.java
+++ src/main/java/com/lojaunit/base/Cliente.java
@@ -2,73 +2,127 @@ package com.lojaunit.base;
 
 import java.sql.Date;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import com.sun.istack.NotNull;
+
+@Entity
+@Table(name = "clientes")
 public class Cliente {
-	
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column
+	@NotNull
 	private Integer id;
+
+	@Column(length = 100)
+	@NotNull
 	private String nome;
+
+	@Column(length = 11, unique = true)
+	@NotNull
 	private String cpf;
+
+	@Column(length = 200)
+	@NotNull
 	private String email;
+
+	@Column
 	private Date dataNascimento;
+
+	@Column(length = 20)
 	private String sexo;
+
+	@Column(length = 100)
 	private String nomeSocial;
+
+	@Column(length = 100)
 	private String apelido;
+
+	@Column(length = 13)
 	private String telefone;
+
+	/**
+	 * Gets e Sets
+	 * 
+	 * @author Cícero Junior
+	 */
+
 	public Integer getId() {
 		return id;
 	}
+
 	public void setId(Integer id) {
 		this.id = id;
 	}
+
 	public String getNome() {
 		return nome;
 	}
+
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+
 	public String getCpf() {
 		return cpf;
 	}
+
 	public void setCpf(String cpf) {
 		this.cpf = cpf;
 	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
 	public Date getDataNascimento() {
 		return dataNascimento;
 	}
+
 	public void setDataNascimento(Date dataNascimento) {
 		this.dataNascimento = dataNascimento;
 	}
+
 	public String getSexo() {
 		return sexo;
 	}
+
 	public void setSexo(String sexo) {
 		this.sexo = sexo;
 	}
+
 	public String getNomeSocial() {
 		return nomeSocial;
 	}
+
 	public void setNomeSocial(String nomeSocial) {
 		this.nomeSocial = nomeSocial;
 	}
+
 	public String getApelido() {
 		return apelido;
 	}
+
 	public void setApelido(String apelido) {
 		this.apelido = apelido;
 	}
+
 	public String getTelefone() {
 		return telefone;
 	}
+
 	public void setTelefone(String telefone) {
 		this.telefone = telefone;
 	}
-	
-	
-	
 
 }
